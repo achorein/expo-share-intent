99cc24df67bcc8c4c31612601b076f7be418a7bc
Anselme Chorein <achorein@users.noreply.github.com>
2024-02-24 19:50:03 +0100

    fix: share-intent-type (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import Constants from "expo-constants";
+
 export { ShareIntent } from "./ExpoShareIntentModule.types";
+
+export const SHARE_EXTENSION_KEY = `${Constants.expoConfig?.scheme}Key`;
+
 export {
   getShareIntent,
   addChangeListener,
   addErrorListener,
 } from "./ExpoShareIntentModule";
+
 export { default as useShareIntent } from "./useShareIntent";
