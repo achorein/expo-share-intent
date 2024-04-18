1a9f169504d8be6be460f4233ceac5fc97f87b94
achorein <anselme@chorein.fr>
2024-04-18 17:38:37 +0200

    update types

--- src/ExpoShareIntentModule.types.ts
+++ src/ExpoShareIntentModule.types.ts
@@ -41,20 +41,26 @@ export type ShareIntentMeta = {
 };
 
 /**
+ * The type of shared content that iOS supports.
+ */
+// export type SharedMediaType = AndroidSharedMediaType | "media" | "weburl";
+
+/**
  * Base type for what shared content is common between both platforms.
  */
 interface BaseShareIntent {
-  text?: string | null;
   meta?: ShareIntentMeta;
+  text?: string | null;
 }
+
 /**
  * Shared intent to represent both platforms.
  */
-export interface ShareIntent extends BaseShareIntent {
+export type ShareIntent = BaseShareIntent & {
   files: ShareIntentFile[] | null;
-  webUrl: string | null;
   type: "media" | "file" | "text" | "weburl" | null;
-}
+  webUrl: string | null;
+};
 
 /**
  * Shared intent type for Android.
@@ -69,18 +75,22 @@ export interface AndroidShareIntent extends BaseShareIntent {
  */
 export interface IosShareIntent extends BaseShareIntent {
   files?: IosShareIntentFile[];
-  type: "media" | "file" | "text" | "weburl" | null;
+  type: "media" | "file" | "text" | "weburl";
 }
 
 /**
  * ShareIntentFile that is common among both platforms
  */
-export interface ShareIntentFile {
+export type ShareIntentFile = {
   fileName: string;
   mimeType: string;
   path: string;
   size: number | null;
-}
+};
+
+/**
+ * ShareIntentFile in iOS
+ */
 export interface IosShareIntentFile {
   fileSize?: number; // in octet
   fileName: string; // original filename
@@ -89,12 +99,15 @@ export interface IosShareIntentFile {
   type: "0" | "1" | "2" | "3"; // native type ("0": media, "1": text, "2": weburl, "3": file)
 }
 
+/**
+ * ShareIntentFile in Android
+ */
 export interface AndroidShareIntentFile {
   contentUri: string; // original android uri of file
+  mimeType: string; // ex: image/png
+  fileName: string; // original filename
   filePath: string; // computed full path of file
   fileSize?: string; // in octet
-  fileName: string; // original filename
-  mimeType: string; // ex: image/png
 }
 
 export type NativeShareIntent = AndroidShareIntent | IosShareIntent;
