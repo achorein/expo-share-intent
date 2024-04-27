35a8a1b22c4dc68fd9aafe1ca5b279fb8a6f46a6
achorein <anselme@chorein.fr>
2024-04-27 09:57:11 +0200

    fix: eas build experimental config

--- plugin/src/ios/withIosShareExtensionConfig.ts
+++ plugin/src/ios/withIosShareExtensionConfig.ts
@@ -12,46 +12,60 @@ export const withShareExtensionConfig: ConfigPlugin = (config) => {
   const shareExtensionIdentifier =
     getShareExtensionBundledIdentifier(appIdentifier);
 
-  let extConfigIndex = null;
-  config.extra?.eas?.build?.experimental?.ios?.appExtensions?.forEach(
-    (ext: any, index: number) => {
-      ext.targetName === extName && (extConfigIndex = index);
-    },
-  );
+  // let extConfigIndex = null;
+  // config.extra?.eas?.build?.experimental?.ios?.appExtensions?.forEach(
+  //   (ext: any, index: number) => {
+  //     ext.targetName === extName && (extConfigIndex = index);
+  //   },
+  // );
 
-  if (!config.extra) {
-    config.extra = {};
-  }
+  // if (!config.extra) {
+  //   config.extra = {};
+  // }
 
-  if (!extConfigIndex) {
-    if (!config.extra.eas) {
-      config.extra.eas = {};
-    }
-    if (!config.extra.eas.build) {
-      config.extra.eas.build = {};
-    }
-    if (!config.extra.eas.build.experimental) {
-      config.extra.eas.build.experimental = {};
-    }
-    if (!config.extra.eas.build.experimental.ios) {
-      config.extra.eas.build.experimental.ios = {};
-    }
-    if (!config.extra.eas.build.experimental.ios.appExtensions) {
-      config.extra.eas.build.experimental.ios.appExtensions = [];
-    }
-    config.extra.eas.build.experimental.ios.appExtensions.push({
+  // if (!extConfigIndex) {
+  //   if (!config.extra.eas) {
+  //     config.extra.eas = {};
+  //   }
+  //   if (!config.extra.eas.build) {
+  //     config.extra.eas.build = {};
+  //   }
+  //   if (!config.extra.eas.build.experimental) {
+  //     config.extra.eas.build.experimental = {};
+  //   }
+  //   if (!config.extra.eas.build.experimental.ios) {
+  //     config.extra.eas.build.experimental.ios = {};
+  //   }
+  //   if (!config.extra.eas.build.experimental.ios.appExtensions) {
+  //     config.extra.eas.build.experimental.ios.appExtensions = [];
+  //   }
+  //   config.extra.eas.build.experimental.ios.appExtensions.push({
+  //     targetName: extName,
+  //     bundleIdentifier: shareExtensionIdentifier,
+  //   });
+  //   extConfigIndex = 0;
+  // }
+
+  // const extConfig =
+  //   config.extra.eas.build.experimental.ios.appExtensions[extConfigIndex];
+  // extConfig.entitlements = {
+  //   ...extConfig.entitlements,
+  //   ...getShareExtensionEntitlements(appIdentifier),
+  // };
+
+  // Assume config.extra.eas is already correctly configured outside this plugin
+  // and should not be modified here.
+  // Initialize necessary local variables for setup but do not alter config
+
+  let appExtensions = [
+    {
       targetName: extName,
       bundleIdentifier: shareExtensionIdentifier,
-    });
-    extConfigIndex = 0;
-  }
+      entitlements: getShareExtensionEntitlements(appIdentifier),
+    },
+  ];
 
-  const extConfig =
-    config.extra.eas.build.experimental.ios.appExtensions[extConfigIndex];
-  extConfig.entitlements = {
-    ...extConfig.entitlements,
-    ...getShareExtensionEntitlements(appIdentifier),
-  };
-
+  // This function no longer alters the config object passed to it
+  // It only returns the original config to satisfy any calling conventions
   return config;
 };
