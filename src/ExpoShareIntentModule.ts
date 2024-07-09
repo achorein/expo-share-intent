b41a90a0d816e5ea1ad6fc9f938d01003b93e1f8
Anselme <anselme@chorein.fr>
2024-07-09 21:34:58 +0200

    update dependencies

--- src/ExpoShareIntentModule.ts
+++ src/ExpoShareIntentModule.ts
@@ -1,6 +1,5 @@
 import {
-  requireNativeModule,
-  NativeModulesProxy,
+  requireOptionalNativeModule,
   EventEmitter,
   Subscription,
 } from "expo-modules-core";
@@ -14,39 +13,43 @@ import { getShareExtensionKey } from "./utils";
 // Import the native module. it will be resolved on native platforms to ExpoShareIntentModule.ts
 // It loads the native module object from the JSI or falls back to
 // the bridge module (from NativeModulesProxy) if the remote debugger is on.
-const ExpoShareIntentModule = requireNativeModule("ExpoShareIntentModule");
+const ExpoShareIntentModule = requireOptionalNativeModule(
+  "ExpoShareIntentModule",
+);
 export default ExpoShareIntentModule;
 
 export function getShareIntent(url = ""): string {
-  return ExpoShareIntentModule.getShareIntent(url);
+  return ExpoShareIntentModule?.getShareIntent(url);
 }
 
 export function clearShareIntent(key: string) {
-  return ExpoShareIntentModule.clearShareIntent(key ?? getShareExtensionKey());
+  return ExpoShareIntentModule?.clearShareIntent(key ?? getShareExtensionKey());
 }
 
 export function hasShareIntent(key: string): boolean {
-  return ExpoShareIntentModule.hasShareIntent(key ?? getShareExtensionKey());
+  return ExpoShareIntentModule?.hasShareIntent(key ?? getShareExtensionKey());
 }
 
-const emitter = new EventEmitter(
-  ExpoShareIntentModule ?? NativeModulesProxy.ExpoShareIntentModule,
-);
+const emitter = ExpoShareIntentModule
+  ? new EventEmitter(ExpoShareIntentModule)
+  : null;
 
 export function addErrorListener(
   listener: (event: ChangeEventPayload) => void,
-): Subscription {
-  return emitter.addListener<ChangeEventPayload>("onError", listener);
+): Subscription | null {
+  return emitter?.addListener<ChangeEventPayload>("onError", listener) || null;
 }
 
 export function addChangeListener(
   listener: (event: ChangeEventPayload) => void,
-): Subscription {
-  return emitter.addListener<ChangeEventPayload>("onChange", listener);
+): Subscription | null {
+  return emitter?.addListener<ChangeEventPayload>("onChange", listener) || null;
 }
 
 export function addStateListener(
   listener: (event: StateEventPayload) => void,
-): Subscription {
-  return emitter.addListener<StateEventPayload>("onStateChange", listener);
+): Subscription | null {
+  return (
+    emitter?.addListener<StateEventPayload>("onStateChange", listener) || null
+  );
 }
