c44657098f6a0ac10a05721fef79faf8c743c931
Mariano Patafio <mariano.patafio@corner.ch>
2026-05-07 17:28:24 +0200

    fix: restore iOS app entitlements source

--- plugin/src/ios/withIosAppEntitlements.ts
+++ plugin/src/ios/withIosAppEntitlements.ts
@@ -1,17 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.withAppEntitlements = void 0;
-const config_plugins_1 = require("@expo/config-plugins");
-const constants_1 = require("./constants");
-const withAppEntitlements = (config, parameters) => {
-    return (0, config_plugins_1.withEntitlementsPlist)(config, async (config) => {
-      const appIdentifier = config.ios?.bundleIdentifier;
-      const existing = config.modResults["com.apple.security.application-groups"] ?? [];
-      config.modResults["com.apple.security.application-groups"] = [
-        (0, constants_1.getAppGroup)(appIdentifier, parameters),
-        ...existing,
-      ];
-      return config;
-    });
+import { ConfigPlugin, withEntitlementsPlist } from "@expo/config-plugins";
+
+import { getAppGroup } from "./constants";
+import { Parameters } from "../types";
+
+export const withAppEntitlements: ConfigPlugin<Parameters> = (
+  config,
+  parameters,
+) => {
+  return withEntitlementsPlist(config, async (config) => {
+    const appIdentifier = config.ios?.bundleIdentifier!;
+    const existing =
+      config.modResults["com.apple.security.application-groups"] ?? [];
+    config.modResults["com.apple.security.application-groups"] = [
+      getAppGroup(appIdentifier, parameters),
+      ...existing,
+    ];
+    return config;
+  });
 };
-exports.withAppEntitlements = withAppEntitlements;
